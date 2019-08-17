1a5f9d8f7fb56b0a1f819d9193fcfca29a74d2ce
TheQuest1 <madshallenberg1@hotmailcom>
2019-08-18 00:10:28 +0200

    Added Terrain, background and Standard assets pakage

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    [SerializeField] float loadTime = 2f;
+
+
+
+    private void Awake()
+    {
+            DontDestroyOnLoad(gameObject);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene", loadTime);
+    }
+
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
