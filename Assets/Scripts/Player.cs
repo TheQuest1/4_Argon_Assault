780805a3eb70f278d91ed1d37ece7737becd8bba
TheQuest1 <madshallenberg1@hotmailcom>
2019-08-18 01:35:59 +0200

    Added Music player and added movement to the ship with Mathf.Clamp

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In Meters pr. seconds")]
+    [SerializeField] float speed = 30f;
+    [Tooltip("In Meters")]
+    [SerializeField] float xRange = 13f;
+    [Tooltip("In Meters")]
+    [SerializeField] float yRange = 9f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+ 
+
+    private void Movement()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float yOffset = yThrow * speed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+}
